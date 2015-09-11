749938b81197a4707b0ca4921e289ee0450b7f42
Vincent Delorme <vincent.delorme@mindcandy.com>
2015-09-11 17:40:19 +0100

    Initial commit

---
+++ rpgmanager/rpgmanager/R/manager.R
@@ -0,0 +1,152 @@
+# Dependencies
+# RPostgreSQL
+if (!"RPostgreSQL" %in% installed.packages()) install.packages("RPostgreSQL")
+library("RPostgreSQL")
+
+setClass("PostgresConnectionManager", slots = c(connection="PostgreSQLConnection", driver="PostgreSQLDriver"))
+
+#' Easy connection to a Postgres database using aliases
+#'
+#' Creates a PostgreSQL driver and a connection to the database \code{dbAlias}
+#' using the .pgpass and db.conf files specified.
+#' 
+#' The db.conf file is defined one alias per line. Each alias is defined using this format:
+#' \code{#alias:hostname:port:database:username}
+#' \code{database1:my.postgres.db.com:5432:my_database1:user1}
+#' @param dbAlias alias of the database you want to connect to (must be present in db.conf)
+#' @param pgPassFile location of ".pgpass" on your filesystem (optional; defaults to "~/.pgpass")
+#' @param dbConfFile location of "db.conf" on your filesystem (optional; defaults to "~/db.conf")
+#' @return PostgresConnectionManager object including a PostgreSQLDriver and a PostgresSQLConnection 
+#'          to the database in question
+#' @seealso \code{\link{runQuery}}, \code{\link{destroyConnection}} 
+#' @export
+#' @examples
+#' newConnection("my_db")
+#' newConnection("my_db", dbConfFile = "/path/to/your/db.conf")
+newConnection <- function(dbAlias, pgPassFile = "~/.pgpass", dbConfFile = "~/db.conf") {
+  # Creates a PostgreSQL driver and a connection to the database `dbAlias`
+  # using the pgpass and db.conf files specified.
+  #
+  # Args:
+  #   dbAlias: Alias of the database you want to connect to (must be
+  #            present in db.conf)
+  #   pgPassFilex: Location of ".pgpass" on your filesystem.
+  #   dbConfFile: Location of "db.conf" on your filesystem.
+  #
+  # Returns:
+  #   A PostgresConnectionManager including a PostgreSQLDriver and a PostgresSQLConnection to the database in question.
+  
+  processFileRegex <- function(filename, regex, header = TRUE) {
+    # Create data frame based on the data from `filename`. Filters out anything that doesn't match the 
+    # regex. Capturing groups define the columns of the data frame.
+    # 
+    # Args:
+    #   filename: Location of the file to be processed.
+    #   regex: Regular expression to process the file with
+    #   header: if TRUE, first row is used as column names and removed from the data frame.
+    #           Defaults to TRUE.
+    #   silent: if TRUE, won't output the logs / reminders after running. Defaults to FALSE.
+    # 
+    # Returns:
+    #   A data frame corresponding to the data from filename matching the regex, broken down in colums.
+    rawFile <- readLines(filename)
+    rawClean <- rawFile[grep(regex, rawFile)]
+    rawFrame <- as.data.frame(str_match(rawClean, regex), stringsAsFactors = FALSE)[,-1]
+    if (header) {
+      colnames(rawFrame) <- as.vector(rawFrame[1,])
+      rawFrame <- rawFrame[-1,]
+    }
+    return(rawFrame)
+  }
+  
+  # Parses passwords aliases files
+  dbPass <- processFileRegex(pgPassFile, "^([^#][^:]*):([^:]*):([^:]*):([^:]*):(.*)$")
+  dbConf <- processFileRegex(dbConfFile, "^([^#][^:]*):([^:]*):([^:]*):([^:]*):([^:]*)$")
+  
+  # Merges the files, also accounts for potential "*" in db
+  dbMerged <- merge(dbPass, dbConf, by=c("hostname", "username", "port"))
+  dbMerged <- dbMerged[(dbMerged$database.x == "*") || (dbMerged$database.x == dbMerged$database.y), !(names(dbMerged) == "database.x")]
+  names(dbMerged)[names(dbMerged) == "database.y"] <- "database"
+  
+  dbInfo <- subset(dbMerged, dbMerged$alias == dbAlias)
+  if (nrow(dbInfo) == 0) stop("Alias provided not found in db.conf")
+  if (nrow(dbInfo) > 1) stop("Error looking up the alias provided in db.conf")
+  
+  drv <- dbDriver("PostgreSQL")
+  con <- dbConnect(drv,
+                   host=dbInfo$hostname,
+                   dbname=dbInfo$database,
+                   user=dbInfo$username,
+                   port=as.integer(dbInfo$port),
+                   password=dbInfo$password)
+  
+  pgObjects <- new("PostgresConnectionManager", 
+                   driver = drv, 
+                   connection = con)
+  
+  return(pgObjects)
+}
+
+#' Clears up the resources used by a PostgresConnectionManager
+#'
+#' Clears up resources by disconnecting all the connections to the driver, 
+#' then unloading the driver
+#' @param pgManager PostgresConnectionManager object to run the query against
+#' @return Nothing. Side-effects only. 
+#' @seealso \code{\link{newConnection}}, \code{\link{runQuery}}
+#' @export
+#' @examples
+#' destroyConnection(pgManager)
+destroyConnection <- function(pgManager) {
+  # Clears up the resources used by a PostgresConnectionManager object by disconnecting all the connections
+  # to the driver, then unloading the driver.
+  #
+  # Args:
+  #   pgManager: A PostgresConnectionManager object
+  #
+  # Returns:
+  #   Nothing. Side-effects only. 
+  if (!(class(pgManager) == "PostgresConnectionManager" && typeof(pgManager) == "S4")) stop("Parameter should be an instance of PostgresConnectionManager")
+  for (conn in dbListConnections(pgManager@driver)) { 
+    dbDisconnect(conn)
+  }
+  dbUnloadDriver(pgManager@driver)
+}
+
+#' Run a query against a PostgresConnectionManager object
+#'
+#' Runs a query against the specified PostgresConnectionManager object, either by specifying the SQL directly or 
+#'pointing at a file containing the query.
+#' @param pgManager PostgresConnectionManager object to run the query against
+#' @param query a string containing query to run against the connection (optional; needed if useFile = FALSE)
+#' @param useFile a boolean indicating whether to run a query directly or run the query from file (optional; defaults to FALSE)
+#'   If TRUE, queryLocation must be present and pointing to an existing file.
+#'   If FALSE, query must be specified.
+#' @param queryLocation a string pointing at the location of the query file (optional; needed if useFile = TRUE)
+#' @return The resulting data frame
+#' @seealso \code{\link{newConnection}}, \code{\link{destroyConnection}} 
+#' @export
+#' @examples
+#' runQuery(pgManager, "SELECT * FROM users;")
+#' runQuery(pgManager, useFile = TRUE, queryLocation = "~/query.sql")
+runQuery <- function(pgManager, query = "", useFile = FALSE, queryLocation = "") {
+  # Runs a query against the specified PostgresConnectionManager object, either by specifying the SQL directly or 
+  # pointing at a file containing the query.
+  #
+  # Args:
+  #   pgManager: A PostgresConnectionManager object
+  #   query: The query to run against the connection (as a string)
+  #   useFile: A boolean indicating whether to run a query directly or run the query from file.
+  #             If TRUE, queryLocation must be present and pointing to an existing file.
+  #             If FALSE, query must be specified.
+  #   queryLocation: Location of the query file on your filesystem.
+  #
+  # Returns:
+  #    The resulting data frame.
+  if (!(class(pgManager) == "PostgresConnectionManager" && typeof(pgManager) == "S4")) stop("Parameter should be an instance of PostgresConnectionManager")
+  if (useFile && !file.exists(queryLocation)) stop("Specified query file doesn't exist")
+  if (useFile) {
+    query <- readChar(queryLocation,nchar = file.info(queryLocation)$size)
+  }
+  return(dbGetQuery(pgManager@connection, query))
+}
